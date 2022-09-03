9150ece1843678b72cb96bbd0d3f5cd638617309
Md. Shakhawat Hossain Shahin <shakhawat.ch@gmail.com>
2022-09-04 00:36:27 +0600

    Initial Commit

---
+++ BS23PracticalTest/BS23PracticalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BS23PracticalTestApp.swift
+//  BS23PracticalTest
+//
+//  Created by Shakhawat Hossain Shahin on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct BS23PracticalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BS23PracticalTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BS23PracticalTest
+//
+//  Created by Shakhawat Hossain Shahin on 4/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
